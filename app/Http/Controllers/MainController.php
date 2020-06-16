eab476e08f8ae6d33b600206a3bfdaef776d5e98
Wyatt Johnson <wyatt.j1834@gmail.com>
2020-06-16 09:11:46 -0600

    Setup authentication

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Auth;
+
+class MainController extends Controller
+{
+    public function getIndex() {
+        return view('other.home');
+    }
+}
