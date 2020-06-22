30f18982dfedd6d956181ed04f387f28175535de
Wyatt Johnson <wyatt.j1834@gmail.com>
2020-06-22 12:06:53 -0600

    Added routes, controllers, migrations

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,22 @@ Route::get('about', 'MainController@getAbout')->name('other.about');
 
 Route::group(['prefix' => 'user', 'middleware' => 'auth'], function() {
     Route::get('/home', 'MainController@getUserIndex')->name('user.home');
+    
+    Route::group(['prefix' => 'gasMileage'], function() {
+        Route::get('/', 'GasController@getGasMileage')->name('gas');
+        Route::get('/edit', 'GasController@getGasEdit')->name('gas.edit');
+        Route::post('/edit', 'GasController@postGasEdit')->name('gas.edit');
+        Route::post('/delete', 'GasController@postGasDelete')->name('gas.delete');
+    });
+
+    Route::group(['prefix' => 'maintenance'], function() {
+        Route::get('/', "MaintController@getMaintenance")->name('maintenance');
+        Route::get('/edit', "MaintController@getMaintenanceEdit")->name('maintenance');
+        Route::post('/edit', "MaintController@postMaintenanceEdit")->name('maintenance');
+        Route::post('/delete', "MaintController@postMaintenanceDelete")->name('maintenance');
+
+    });
+
 });
 
 Auth::routes();
